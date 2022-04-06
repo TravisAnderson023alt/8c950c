d6f7392cc6b5a2d878cded5ea58b32669f05bd76
Spotexx <tsanderson.023@gmail.com>
2022-04-06 13:07:29 -0400

    updated git ignore, added concurrently for easier launching of develop, corrected connection in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,14 @@
 const Sequelize = require("sequelize");
-
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+require("dotenv").config();
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432,
+  }
+);
 
 module.exports = db;
